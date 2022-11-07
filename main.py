19a1a15a5dfc8f6e832005a0ebbff2b5a252872f
Chloe <chloe_rowe@outlook.com>
2022-11-07 17:24:12 +0000

    Commit 2
    Added Manage Products / Add a Product

--- main.py
+++ main.py
@@ -28,14 +28,17 @@ quotes_menu_text = """
 4. Return to Main Menu
 """
 
+creating_new_product_text = """
+------------------------------------------
+:: Creating a New Product
+------------------------------------------
+"""
+
 def get_user_option():
     user_option = input("Please select a menu option: ")
     if user_option.isdigit():
         user_option = int(user_option)
     return user_option
-
-def main_menu_option(user_option):
-
 
 
 display_main_menu = True
@@ -48,7 +51,7 @@ while display_main_menu:
     if menu_option <= 3:
         display_main_menu = False
 
-        if menu_option == 1: # MANAGE MORTGAGE PRODUCTS
+        if menu_option == 1: # SELECTS MANAGE MORTGAGE PRODUCTS
             display_products_menu = True
 
             while display_products_menu:
@@ -58,18 +61,46 @@ while display_main_menu:
                 if menu_option <= 5:
                     display_products_menu = False
 
-                    if menu_option == 1: # ADD PRODUCT
+                    if menu_option == 1: # PRODUCTS SUBMENU: SELECTS ADD PRODUCT
+                        print(creating_new_product_text)
+                        product_name = input("Please input product name : ")
+                        product_interest_rate = input("Please input the interest rate for this product : ")
+                        product_details = [product_name, product_interest_rate]
 
-                    elif menu_option == 2: # VIEW PRODUCTS
+                        print(f"""
+                        ==================================================
+                        Product Summary
+                        ==================================================
+                        Product name : {product_name}
+                        Product rate : {product_interest_rate}
+                        --------------------------------------------------
+                        """)
 
-                    elif menu_option == 3: # AMEND PRODUCT
+                        product_details_confirmation = input("Please confirm the product's details (input 1) to "
+                                                             "proceed with "
+                                                             "product creation (or any key to quit : ")
 
-                    elif menu_option == 4: # DELETE PRODUCT
+                        print(f"""
+                        Creating New Product
+                        Generating new product........
+                        Generated product name : {product_name}
+                        Generated product rate : {product_interest_rate}
+                        """)
+                        input("Press enter to continue")
+
+                    elif menu_option == 2: # PRODUCTS SUBMENU: VIEW PRODUCTS
+                        print("You selected 2")
+
+                    elif menu_option == 3: # PRODUCTS SUBMENU: AMEND PRODUCT
+                        print("You selected 3")
+
+                    elif menu_option == 4: # PRODUCTS SUBMENU: DELETE PRODUCT
+                        print("You selected 4")
 
                     else: # RETURN TO MAIN MENU
                         display_main_menu = True
 
-        elif menu_option == 2: # MANAGE MORTAGGE QUOTES
+        elif menu_option == 2: # MANAGE MORTGAGE QUOTES
             display_quotes_menu = True
 
             # Displays Sub-Menu
